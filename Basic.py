d5bf845dccca96ca4aa4641d11ff66cc72f209c1
zj-work <zj_work01@outlook.com>
2019-04-13 22:38:53 +0800

    basic learning

---
+++ Basic.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3.6
+# -*- coding: UTF-8 -*-
+
---
+++ Index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.6
+# -*- coding: UTF-8 -*-
+
+str = '''
+1.字符串处理
+2.数字格式化处理
+3.时间格式处理
+4.数据类型之间的转换
+5.list和tuple的使用
+6.如何将list转换为tuple
+7.字典和集合的使用
+'''
+print(str)
